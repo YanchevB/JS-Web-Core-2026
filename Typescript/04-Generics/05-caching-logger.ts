c62b7acb5587430574d616a18ed5365d5d313207
Boyan Yanchev <byanchev6@gmail.com>
2026-03-24 21:56:48 +0200

    Solve exercise 5

--- Typescript/04-Generics/05-caching-logger.ts
+++ Typescript/04-Generics/05-caching-logger.ts
@@ -15,3 +15,56 @@ interface CachingLogger<T extends LoggingLevel, V extends LoggingFormat> {
     log(logLevel: T, message: string): void;
     getFormat(): V
 }
+
+class Logger<T extends LoggingLevel, V extends LoggingFormat> implements CachingLogger<T, V> {
+    cachedLogs: Map<T, string[]> = new Map();
+    private format: V;
+
+    constructor(format: V) {
+        this.format = format;
+    }
+
+    log(logLevel: T, message: string): void {
+        const date = new Date().toISOString();
+        const formattedMessage = this.format
+            .replace('%level', logLevel)
+            .replace('%date', date)
+            .replace('%text', message);
+
+        console.log(formattedMessage);
+
+        if (this.cachedLogs.has(logLevel)) {
+            const currentMessages = this.cachedLogs.get(logLevel)!;
+            currentMessages.push(formattedMessage);
+        } else {
+            this.cachedLogs.set(logLevel, [formattedMessage]);
+        }
+    }
+
+    getFormat(): V {
+        return this.format;
+    }
+}
+
+// let logger = new Logger<LoggingLevel, LoggingFormat>(LoggingFormat.Standard);
+
+// logger.log(LoggingLevel.Info, "This is an info message.");
+// logger.log(LoggingLevel.Info, "Another message.");
+// logger.log(LoggingLevel.Error, "Something went wrong.");
+// logger.log(LoggingLevel.Warning, "Be careful with the type assertions.");
+// logger.log(LoggingLevel.Debug, "Running the debugger.");
+
+// console.log('-----------')
+// console.log([...logger.cachedLogs.entries()].map(x => x[1].join('\n')).join('\n'))
+
+let logger = new Logger<LoggingLevel, LoggingFormat>(LoggingFormat.Minimal);
+logger.log(LoggingLevel.Info, "Just a simple message.");
+logger.log(LoggingLevel.Error, "A Problem happened.");
+console.log('-----------')
+console.log(logger.getFormat());
+console.log([...logger.cachedLogs.entries()].map(x => x[1].join('\n')).join('\n'))
+
+// let logger = new Logger<LoggingLevel, LoggingFormat>("%text"); //TS Error
+// let wronglogger = new Logger<string, LoggingLevel>(); //TS Error
+// logger.log("%s", "Running the debugger."); //TS Error
+// logger.log({format: "Test %s"}, "Running the debugger."); //TS Error
