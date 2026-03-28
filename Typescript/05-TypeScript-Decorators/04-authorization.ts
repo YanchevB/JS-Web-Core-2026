b368cb9848ee3c2973b1cf048714a5bc01c403df
Boyan Yanchev <byanchev6@gmail.com>
2026-03-28 16:36:37 +0200

    Solve exercise 4

--- Typescript/05-TypeScript-Decorators/04-authorization.ts
+++ Typescript/05-TypeScript-Decorators/04-authorization.ts
@@ -1,5 +1,21 @@
 
-//TODO: Solve exercise
+function authorizeUser(service: MockAuthorizationService) {
+    return function(
+        target: object, 
+        propName: string, 
+        descriptor: PropertyDescriptor
+    ): PropertyDescriptor {
+        const originalGet = descriptor.get!;
+
+        descriptor.get = function() {
+            if (!service.canViewData(propName)) {
+                throw new Error('You are not authorized to view this information');
+            } 
+            return originalGet.call(this);
+        }
+        return descriptor;
+    }
+}
 
 class MockAuthorizationService {
     constructor(private userRole: 'Guest' | 'PersonalDataAdministrator' | 'Admin') { }
@@ -13,3 +29,37 @@ class MockAuthorizationService {
         }
     }
 }
+
+let mockAuthorizationService = new MockAuthorizationService('Guest');
+
+class User5 {
+    private _name: string;
+    private _age: number;
+    private _creditCardNumber: string
+
+    constructor(name: string, age: number, creditCardNumber: string) {
+        this._name = name;
+        this._age = age;
+        this._creditCardNumber = creditCardNumber;
+    }
+    
+    @authorizeUser(mockAuthorizationService)
+    public get name(): string {
+        return this._name;
+    }
+
+    @authorizeUser(mockAuthorizationService)
+    public get age(): number {
+        return this._age;
+    }
+
+    @authorizeUser(mockAuthorizationService)
+    public get creditCardNumber(): string {
+        return this._creditCardNumber;
+    }
+}
+
+const user5 = new User5("John Doe", 30, 'ABCD-1234');
+console.log(user5.name);
+console.log(user5.age);
+console.log(user5.creditCardNumber);
